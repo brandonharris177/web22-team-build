5e4994d8dfd8479410c201a5705438a1e67866fe
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-18 12:54:20 -0500

    added my stuff

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Stuff and Things and Aliens in the Sky</h1>
       <h2>
         Deployed with{' '}
         <a
